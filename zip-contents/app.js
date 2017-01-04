92f1aa116a24362d3645004d47b4a1050c8c08f3
Barry Wark <barrywark@gmail.com>
2017-01-04 13:43:55 -0500

    Parse cookies.
    You dumb idiot.

--- zip-contents/app.js
+++ zip-contents/app.js
@@ -25,7 +25,7 @@ app.use(bodyParser.urlencoded({
   extended: false
 }));
 app.use(helmet());
-// app.use(cookieParser());
+app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 //enable CORS
